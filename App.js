1af768234046b008ca8d0bb07e94f0cf5c0764e8
isdiscodead <isdiscodead@korea.ac.kr>
2021-12-11 19:28:49 +0900

    preload images

--- App.js
+++ App.js
@@ -1,15 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, Image } from 'react-native';
+
+import AppLoading from "expo-app-loading";
+import { Asset } from "expo-asset"; 
+
+
+const cacheImages = images => images.map(image => {
+  if(typeof image === "string") { // 주소의 경우
+    return Image.prefetch(image)
+  } else {
+    return Asset.fromModule(image).downloadAsync();
+  }
+})
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAssets = () => {
+      // image와 font preload
+      const images = [require("./assets/loginBg.jpeg"), "https://ebenezersuites.com/wp-content/uploads/2016/06/airbnb-logo-266x300@2x.png"];
+      console.log(cacheImages(images));
+  }
+
+  return isReady ? (
+  <Text>I'm ready</Text>
+  ) : ( <AppLoading 
+          onError={console.error} 
+          onFinish={handleFinish} 
+          startAsync={loadAssets}
+        />
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
