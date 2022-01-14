3bdc8b7a8ce5809f58b77dc2ce0150f921c5d714
isdiscodead <isdiscodead@korea.ac.kr>
2022-01-14 23:20:54 +0900

    add btn to screen

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import styled from "styled-components/native";
 import { BlurView } from 'expo-blur'; 
+import Btn from "../components/Auth/Btn";
 
 const Container = styled.View`
     flex: 1;
@@ -23,13 +24,25 @@ const Logo = styled.Image`
 
 const LOGO_URL = "https://ebenezersuites.com/wp-content/uploads/2016/06/airbnb-logo-266x300@2x.png";
 
+const BtnContainer = styled.View``;
+
 export default ({ navigation }) => {
+    // 버튼 function
+    const goToSignUp = () => navigation.navigate("SignUp")
+    const goToSignIn = () => navigation.navigate("SignIn")
+
     return (
         <Container>
             <Image resizeMethod="scale" source={require('../assets/loginBg.jpeg')} /> 
+
             <BlurView intensity={35} tint="light" style={{ flex: 1, width: "100%", alignItems: "center", justifyContent: "center"}}>
                 <Logo source={{ uri: LOGO_URL }} /> 
+                <BtnContainer>
+                    <Btn onPress={"goToSignUp"} text={"Sign Up"} accent={true} />
+                    <Btn onPress={"goToSignIn"} text={"Sign In"} />
+                </BtnContainer>
             </BlurView>
+
             <StatusBar style="light"/>
         </Container>
     );
